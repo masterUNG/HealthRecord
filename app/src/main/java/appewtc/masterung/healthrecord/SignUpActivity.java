9df5662e8d7ec0e4713779b49042ce1903a2c8e4
masterUNG <phrombutr@gmail.com>
2015-10-09 13:48:40 +0700

    Create AlertDialog

--- app/src/main/java/appewtc/masterung/healthrecord/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/healthrecord/SignUpActivity.java
@@ -2,6 +2,7 @@ package appewtc.masterung.healthrecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+    }   // onCreate
+
+    public void clickSave(View view) {
+
+    }   // clickSave
+
+
+}   // Main Class
