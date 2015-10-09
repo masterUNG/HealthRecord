7839cb189c017e280470b3857679715f2121ebc4
masterUNG <phrombutr@gmail.com>
2015-10-09 14:19:24 +0700

    Design RecordActivity success

--- app/src/main/java/appewtc/masterung/healthrecord/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/healthrecord/SignUpActivity.java
@@ -3,16 +3,35 @@ package appewtc.masterung.healthrecord;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText, nameEditText,
+            emailEditText, weightEditText, heightEditText;
+    private RadioGroup sexRadioGroup;
+    private Spinner ageSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+
     }   // onCreate
 
+    private void bindWidget() {
+
+    }
+
     public void clickSave(View view) {
+
+
 
     }   // clickSave
 
