933f54ec25c105c1a9c76e560142fccaab5809d9
masterUNG <phrombutr@gmail.com>
2015-10-29 16:12:23 +0700

    Get Current Time

--- app/src/main/java/appewtc/masterung/healthrecord/RecordActivity.java
+++ app/src/main/java/appewtc/masterung/healthrecord/RecordActivity.java
@@ -2,12 +2,41 @@ package appewtc.masterung.healthrecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class RecordActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView showTimeTextView;
+    private String currentTimeString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        //Bind Widget
+        bindWidget();
+
+        //Show Time
+        showTime();
+
+    }   // onCreate
+
+    private void showTime() {
+
+        DateFormat objDateFormat = new SimpleDateFormat("yyyy/MM/dd HH/mm/ss");
+        Date currentDate = new Date();
+        currentTimeString = objDateFormat.format(currentDate);
+        showTimeTextView.setText(currentTimeString);
     }
-}
+
+    private void bindWidget() {
+        showTimeTextView = (TextView) findViewById(R.id.txtShowTime);
+    }
+
+}   // Main Class
